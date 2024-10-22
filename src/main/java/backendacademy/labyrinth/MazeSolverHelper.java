85974d61a57968a9a34de9b134b746c74ef9ff54
Alina Zhuravleva <A.Zhuravleva13@yandex.ru>
2024-10-22 17:44:16 +0300

    Corrected all comments except documentation

---
+++ src/main/java/backendacademy/labyrinth/MazeSolverHelper.java
@@ -0,0 +1,22 @@
+package backendacademy.labyrinth;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public abstract class MazeSolverHelper implements MazeHelper {
+    protected static char[][] maze;
+
+    protected abstract ArrayList<Cell> solver(Maze trueMaze);
+
+    protected static char[][] mazeCopy(char[][] trueMaze) {
+        char[][] mazeCopy = new char[trueMaze.length][];
+        for (int i = 0; i < trueMaze.length; i++) {
+            mazeCopy[i] = Arrays.copyOf(trueMaze[i], trueMaze[i].length);
+        }
+        return mazeCopy;
+    }
+
+    protected static boolean cellIsValid(int x, int y) {
+        return x > 0 && x < maze[0].length && y > 0 && y < maze.length && maze[y][x] == WAY;
+    }
+}
